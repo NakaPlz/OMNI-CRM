7c246e09184c5539844d290d2dfe568c958d8b28
Juan <juan@risut-crm.com>
2025-11-26 02:12:15 -0300

    Fix: Restore App.jsx and add Dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Chats from './pages/Chats';
 import Contacts from './pages/Contacts';
 import Settings from './pages/Settings';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import AuthGuard from './components/AuthGuard';
 import { ChatProvider } from './context/ChatContext';
 import { AuthProvider } from './context/UserAuthContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <ChatProvider>
+    <AuthProvider>
+      <ChatProvider>
+        <Router>
           <Routes>
             {/* Public Routes */}
             <Route path="/login" element={<Login />} />
 
             {/* Protected Routes */}
+            <Route path="/dashboard" element={
+              <AuthGuard>
+                <Layout>
+                  <Dashboard />
+                </Layout>
+              </AuthGuard>
+            } />
             <Route path="/" element={
               <AuthGuard>
                 <Layout>
@@ -44,9 +52,9 @@ function App() {
             {/* Catch all */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
-        </ChatProvider>
-      </AuthProvider>
-    </BrowserRouter>
+        </Router>
+      </ChatProvider>
+    </AuthProvider>
   );
 }
 
